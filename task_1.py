8ccea9f642fc41d691fa7ed73a4d296d58e2fb4a
GkEgor <rws19.93@mail.ru>
2022-05-23 19:40:44 +0300

    ошибка

---
+++ task_1.py
@@ -0,0 +1,15 @@
+
+def request_tuple(file):
+    some_file = open(file, 'r', encoding='utf-8')
+    some_list = []
+    for line in some_file:
+        remote_addr = line[:line.find('-') - 1]
+        request_type = line[line.find('"') + 1:line.find('"') + 4]
+        requested_resource = line[line.find('/d'):line.find('_') + 2]
+        some_tuple = (remote_addr, request_type, requested_resource)
+        # print(some_tuple)
+        some_list.append(some_tuple)
+    print(some_list)
+    some_file.close()
+
+request_tuple('nginx_logs.txt')
