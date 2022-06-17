e19dcb22f0f72ba3dcf8bd468e353d211b1c2532
GkEgor <rws19.93@mail.ru>
2022-06-17 12:50:28 +0300

    task_1.0 исправил метод _ на __

--- task_1.0.py
+++ task_1.0.py
@@ -2,18 +2,16 @@ from time import sleep
 
 class Traffic_lights:
 
-    _color = ['красный', 'желтый', 'зеленый']
+    __color = ['красный', 'желтый', 'зеленый']
 
     def running(self):
-        for i in Traffic_lights._color:
-            print(f'Сигнал светофора - {Traffic_lights._color[0]}')
+            print(f'Сигнал светофора - {Traffic_lights.__color[0]}')
             sleep(7)
-            print(f'Сигнал светофора - {Traffic_lights._color[1]}')
+            print(f'Сигнал светофора - {Traffic_lights.__color[1]}')
             sleep(2)
-            print(f'Сигнал светофора - {Traffic_lights._color[2]}')
-            break
+            print(f'Сигнал светофора - {Traffic_lights.__color[2]}')
 
-traffic_light = Traffic_lights.running('_color')
 
+traffic_light = Traffic_lights.running('__color')
 
 
